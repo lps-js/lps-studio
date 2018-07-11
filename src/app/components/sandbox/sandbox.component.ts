df82cc8722412bd4c0035372510e5a20bbb2cc6d
Sam-Mauris Yong <mauris@hotmail.sg>
2018-07-11 14:28:07 +0100

    add support for sandbox canvas width height changes

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -19,16 +19,41 @@ export class SandboxComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() objects: Array<CanvasObject> = new Array<CanvasObject>();
   @Output() clicked = new EventEmitter<Object>();
   @Output() ready = new EventEmitter();
+  
+  private _width: number = 400;
+  private _height: number = 400;
 
   constructor() {
+  }
+  
+  get width(): number {
+    // transform value for display
+    return this._width;
+  }
+
+  @Input()
+  set width(name: number) {
+    this._width = name;
+    this.mainCanvas.nativeElement.width = this._width;
+  }
+    
+  get height(): number {
+    // transform value for display
+    return this._height;
+  }
+
+  @Input()
+  set height(height: number) {
+    this._height = height;
+    this.mainCanvas.nativeElement.height = this._height;
   }
 
   ngOnInit() {}
   
   ngAfterViewInit() {
     setTimeout(() => {
-      this.mainCanvas.nativeElement.width = 400;
-      this.mainCanvas.nativeElement.height = 400;
+      this.mainCanvas.nativeElement.width = this._width;
+      this.mainCanvas.nativeElement.height = this._height;
       this.context = (<HTMLCanvasElement>this.mainCanvas.nativeElement).getContext('2d');
       this.ready.emit();
       this.canvasDraw();
