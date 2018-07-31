58560d4c2de5e315df7f90cb8bd7185ebb394ce9
Sam-Mauris Yong <mauris@hotmail.sg>
2018-08-01 00:09:22 +0100

    remove click observation console output

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -364,7 +364,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (input === '') {
       return;
     }
-    this.consoleLog('Observing "' + input + '"');
     ipcRenderer.send('lps:observe', { input: input });
   }
   
