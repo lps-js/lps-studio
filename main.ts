d52322b3af4db19fee84cc458a98888021b0e859
Sam Yong <samyonggit@gmail.com>
2018-08-13 20:06:11 +0100

    update LPS api change

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = '1';
 ipcMain.on('lps:start', (event, arg) => {
   let sender = event.sender;
   
-  LPS.load(arg)
+  LPS.loadFile(arg)
     .then((engine) => {
       let queryResult = engine.query(LPS.literal('load_image(Id, Url)'));
       queryResult.forEach((imageTuple) => {
