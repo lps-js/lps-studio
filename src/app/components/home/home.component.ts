3fa3fc24eff3d06ce33a3ac076b6fb714514c4d5
Sam Yong <samyonggit@gmail.com>
2018-08-26 14:41:53 +0100

    update home component to use canvas object service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,247 +1,133 @@
 import { Component, OnInit, OnDestroy, ViewChild, HostListener, ElementRef } from '@angular/core';
 import { ipcRenderer } from 'electron';
 import { SandboxComponent } from '../sandbox/sandbox.component';
-import { Circle } from '../sandbox/canvas/Circle';
-import { Image as ImageObject } from '../sandbox/canvas/Image';
 import { CanvasObject } from '../sandbox/canvas/CanvasObject';
 import { ElectronService } from '../../providers/electron.service';
+import { CanvasObjectService } from '../../providers/canvasObject.service';
 import { OpenDialogOptions } from 'electron';
-const path = require('path');
-  
+import * as path from 'path';
+
 const timebarHeight = 45; // px
-  
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  
   @ViewChild('consoleOutput') consoleOutputView: ElementRef;
-  
+
   messages: Array<string> = [];
-  
+
   consoleInput: String;
   currentTime: string = 'No program loaded';
-  
-  private objects: Object = {};
-  private images: Object = {};
-  private isDone: boolean = false;
+
+  isDone: boolean = false;
   isPaused: boolean = false;
   isRunning: boolean = false;
-  private isMouseDown: boolean = false;
+  isMouseDown: boolean = false;
+
   currentFile: string;
   private LPS;
-  
+
   @ViewChild('sandbox') sandbox: SandboxComponent;
   constructor(
-    private electronService: ElectronService
+    private electronService: ElectronService,
+    private canvasObjectService: CanvasObjectService
   ) {
     this.LPS = this.electronService.remote.require('lps');
   }
 
   ngOnInit() {
+    ipcRenderer.on('canvas:lpsStart', (event, arg) => {
+      this.isRunning = true;
+    });
+
+    ipcRenderer.on('lps-error', (event, arg) => {
+      this.consoleLog('Error: ' + arg);
+      this.isRunning = false;
+    });
+
     ipcRenderer.on('done', (event, arg) => {
       this.isDone = true;
       this.isRunning = false;
       this.currentTime = 'Done';
       this.consoleLog('LPS Program execution complete');
     });
-    
-    ipcRenderer.on('load-image', (event, arg) => {
-      let image = new Image();
-      image.src = arg.url;
-      this.images[arg.id] = image;
+
+    ipcRenderer.on('canvas:loadImage', (event, arg) => {
+      let image = this.canvasObjectService.addImage(arg.id, arg.url);
+      image.onerror = () => {
+        this.sandbox.objects.splice(0, this.sandbox.objects.length);
+        this.canvasObjectService.removeImage(arg.id);
+        this.requestStop();
+        this.consoleLog('Error: Unable to load image ' + arg.id + ' from ' + arg.url);
+      };
     });
-    
-    ipcRenderer.on('time-update', (event, arg) => {
+
+    ipcRenderer.on('canvas:defineObject', (event, arg) => {
+      let obj = this.canvasObjectService.buildObject(arg);
+      if (obj === null) {
+        this.requestStop();
+        this.consoleLog('Error: Invalid object declaration given for ' + arg.id);
+        return;
+      }
+      if (arg.id !== null) {
+        this.canvasObjectService.registerObject(arg.id, obj);
+      }
+      this.sandbox.objects.push(obj);
+    });
+
+    ipcRenderer.on('canvas:updateObject', (event, arg) => {
+      let id = arg.id;
+      let obj = this.canvasObjectService.getObject(id);
+      this.canvasObjectService.updateProperties(obj, arg.properties);
+    });
+
+    ipcRenderer.on('canvas:lpsTimeUpdate', (event, arg) => {
       let time = arg.time;
       this.currentTime = time;
       this.consoleLog('Time ' + time);
     });
-    
-    ipcRenderer.on('draw-image', (event, arg) => {
-      this.objects[arg.id] = new ImageObject(arg.x, arg.y, arg.width, arg.height, this.images[arg.imageId]);
-      this.sandbox.objects.push(this.objects[arg.id]);
-    });
-    
-    ipcRenderer.on('draw-circle', (event, arg) => {
-      this.objects[arg.id] = new Circle(arg.x, arg.y, arg.radius);
-      this.sandbox.objects.push(this.objects[arg.id]);
-    });
-    
-    ipcRenderer.on('hide-object', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <CanvasObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.animations.push(() => {
-          obj.isHidden = true;
-          this.consoleLog('Hiding "' + arg.id + '"');
-          return false;
-        });
-      }, arg.cycleInterval);
-    });
-    
-    ipcRenderer.on('show-object', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <CanvasObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.isHidden = false;
-        this.consoleLog('Showing "' + arg.id + '"');
-      }, arg.cycleInterval);
-    });
-    
-    ipcRenderer.on('enable-drag', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <CanvasObject>this.objects[arg.id];
-      if (this.sandbox.isMouseDown) {
-        obj.isDragEnabled = true;
-      }
-    });
-    
-    ipcRenderer.on('flip-vertical', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <ImageObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.flipVertical = !obj.flipVertical;
-        this.consoleLog('Flipping "' + arg.id + '" Vertically');
-      }, 50);
-    });
-    
-    ipcRenderer.on('clear-flip-vertical', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <ImageObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.flipVertical = false;
-        this.consoleLog('Clear "' + arg.id + '" Vertical Flip');
-      }, 50);
-    });
-    
-    ipcRenderer.on('set-flip-vertical', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <ImageObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.flipVertical = true;
-        this.consoleLog('Set "' + arg.id + '" Vertical Flip');
-      }, 50);
-    });
-    
-    ipcRenderer.on('flip-horizontal', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <ImageObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.flipHorizontal = !obj.flipHorizontal;
-        this.consoleLog('Flipping "' + arg.id + '" Horizontally');
-      }, 50);
-    });
-    
-    ipcRenderer.on('clear-flip-horizontal', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <ImageObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.flipHorizontal = false;
-        this.consoleLog('Clear "' + arg.id + '" Horizontal Flip');
-      }, 50);
-    });
-    
-    ipcRenderer.on('set-flip-horizontal', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <ImageObject>this.objects[arg.id];
-      setTimeout(() => {
-        obj.flipHorizontal = true;
-        this.consoleLog('Set "' + arg.id + '" Horizontal Flip');
-      }, 50);
-    });
-    
-    ipcRenderer.on('move', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = <CanvasObject>this.objects[arg.id];
-      let origin = {
-        x: obj.x,
-        y: obj.y
-      };
-      let deltaX = arg.x - origin.x;
-      let deltaY = arg.y - origin.y;
-      let iteration = 0;
-      this.consoleLog('Start moving "' + arg.id + '" from (' + origin.x + ', ' + origin.y + ')');
-      let startTime;
-      let animateFunc = (timestamp) => {
-        if (startTime === undefined) {
-          startTime = timestamp;
-        }
-        let duration = timestamp - startTime;
-        if (duration >= arg.cycleInterval) {
-          obj.x = arg.x;
-          obj.y = arg.y;
-          this.consoleLog('End moving "' + arg.id + '" to (' + arg.x + ', ' + arg.y + ')');
-          return false;
-        }
-        let ratio = duration / arg.cycleInterval;
-        obj.x = origin.x + ratio * deltaX;
-        obj.y = origin.y + ratio * deltaY;
-        iteration += 1;
-        return true;
-      };
-      obj.animations.push(animateFunc);
-    });
-    
-    ipcRenderer.on('move-to', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = this.objects[arg.id];
-      obj.x = arg.x;
-      obj.y = arg.y;
-    });
-    
-    ipcRenderer.on('move-by', (event, arg) => {
-      if (this.objects[arg.id] === undefined) {
-        return;
-      }
-      let obj = this.objects[arg.id];
-      let deltaX = arg.x;
-      let deltaY = arg.y;
-      let intervalTime = 20;
-      let numCycles = arg.cycleInterval / intervalTime;
-      let deltaXPerIteration = deltaX / numCycles;
-      let deltaYPerIteration = deltaY / numCycles;
-      let iteration = 0;
-      let animateFunc = () => {
-        if (iteration >= numCycles) {
-          return false;
-        }
-        obj.x += deltaXPerIteration;
-        obj.y += deltaYPerIteration;
-        iteration += 1;
-        return true;
-      };
-      obj.animations.push(animateFunc);
-    });
-    
+
+    // ipcRenderer.on('move', (event, arg) => {
+    //   if (this.objects[arg.id] === undefined) {
+    //     return;
+    //   }
+    //   let obj = <CanvasObject>this.objects[arg.id];
+    //   let origin = {
+    //     x: obj.x,
+    //     y: obj.y
+    //   };
+    //   let deltaX = arg.x - origin.x;
+    //   let deltaY = arg.y - origin.y;
+    //   let iteration = 0;
+    //   this.consoleLog('Start moving "' + arg.id + '" from (' + origin.x + ', ' + origin.y + ')');
+    //   let startTime;
+    //   let animateFunc = (timestamp) => {
+    //     if (startTime === undefined) {
+    //       startTime = timestamp;
+    //     }
+    //     let duration = timestamp - startTime;
+    //     if (duration >= arg.cycleInterval) {
+    //       obj.x = arg.x;
+    //       obj.y = arg.y;
+    //       this.consoleLog('End moving "' + arg.id + '" to (' + arg.x + ', ' + arg.y + ')');
+    //       return false;
+    //     }
+    //     let ratio = duration / arg.cycleInterval;
+    //     obj.x = origin.x + ratio * deltaX;
+    //     obj.y = origin.y + ratio * deltaY;
+    //     iteration += 1;
+    //     return true;
+    //   };
+    //   obj.animations.push(animateFunc);
+    // });
+
     this.sandbox.width = window.innerWidth;
     this.sandbox.height = window.innerHeight - timebarHeight;
   }
-  
+
   handleCanvasMouseEvent(e: any) {
     if (!this.isRunning || this.isDone) {
       return;
@@ -250,22 +136,15 @@ export class HomeComponent implements OnInit, OnDestroy {
     let observations = [];
     let observation: any;
     let theta: any;
-    
+
     let forEachObjectInPosition = (callback) => {
-      Object.keys(this.objects).forEach((key) => {
-        let obj = this.objects[key];
-        let halfWidth = obj.width / 2;
-        let halfHeight = obj.height / 2;
-        let minX = obj.x - halfWidth;
-        let maxX = obj.x + halfWidth;
-        let minY = obj.y - halfHeight;
-        let maxY = obj.y + halfHeight;
-        if (e.x >= minX && e.x <= maxX && e.y >= minY && e.y <= maxY) {
+      this.canvasObjectService.iterateObjects((key: string, obj: CanvasObject) => {
+        if (obj.isPositionHit(e.x, e.y)) {
           callback(key, obj);
         }
       });
     }
-    
+
     switch (eventName) {
       case 'click':
         observation = this.LPS.literal('click(X, Y)');
@@ -299,19 +178,18 @@ export class HomeComponent implements OnInit, OnDestroy {
           X: e.x,
           Y: e.y
         };
-        Object.keys(this.objects).forEach((key) => {
-          let obj = this.objects[key];
+        this.canvasObjectService.iterateObjects((key, obj) => {
           if (!obj.isDragEnabled) {
             return;
           }
           // was dragging
-          
+
           observation = this.LPS.literal('draggedTo(ObjectId, X, Y)');
           theta.ObjectId = key;
           observations.push(observation.substitute(theta));
           obj.isDragEnabled = false;
         });
-        
+
         observation = this.LPS.literal('mouseup(X, Y)');
         theta = {
           X: e.x,
@@ -327,8 +205,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       case 'mousemove':
         if (this.sandbox.isMouseDown) {
           // possibly dragging?
-          Object.keys(this.objects).forEach((key) => {
-            let obj = this.objects[key];
+          this.canvasObjectService.iterateObjects((key, obj) => {
             if (!obj.isDragEnabled) {
               return;
             }
@@ -339,7 +216,7 @@ export class HomeComponent implements OnInit, OnDestroy {
             });
           });
         }
-        
+
         observation = this.LPS.literal('mousemove(X, Y)');
         theta = {
           X: e.x,
@@ -353,7 +230,7 @@ export class HomeComponent implements OnInit, OnDestroy {
         });
         break;
     }
-    
+
     let input = '';
     observations.forEach((observation) => {
       if (input !== '') {
@@ -366,7 +243,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
     ipcRenderer.send('lps:observe', { input: input });
   }
-  
+
   consoleLog(message: string) {
     let element = this.consoleOutputView.nativeElement;
     if (element.scrollHeight - element.scrollTop === element.clientHeight) {
@@ -376,10 +253,10 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
     this.messages.push(message);
   }
-  
+
   handleCanvasReady() {
   }
-  
+
   requestPause() {
     if (!this.isRunning || this.isPaused) {
       return;
@@ -388,7 +265,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     ipcRenderer.send('lps:pause');
     this.consoleLog('Pausing LPS program execution...');
   }
-  
+
   requestResume() {
     if (!this.isRunning || !this.isPaused) {
       return;
@@ -397,36 +274,35 @@ export class HomeComponent implements OnInit, OnDestroy {
     ipcRenderer.send('lps:unpause');
     this.consoleLog('Resuming LPS program execution...');
   }
-  
+
   requestStop() {
     if (!this.isRunning) {
       return;
     }
     this.isPaused = false;
-    ipcRenderer.send('lps:terminate');
+    ipcRenderer.send('lps:halt');
     this.consoleLog('Stopping LPS program execution...');
   }
-  
+
   requestRestart() {
     if (this.isRunning || this.currentFile === undefined) {
       return;
     }
-    
+
     const name = path.basename(this.currentFile);
-    
-    this.images = {};
-    this.objects = {};
+
+    this.canvasObjectService.reset();
     this.sandbox.objects = [];
-     
+
     ipcRenderer.send('lps:start', this.currentFile);
-    
+
     this.currentTime = 'Loading ' + name;
     this.consoleLog('Restarting ' + name);
     this.isRunning = true;
     this.isPaused = false;
     this.isDone = false;
   }
-  
+
   requestOpenFile() {
     const dialog = this.electronService.remote.dialog;
     let options: OpenDialogOptions = {
@@ -442,18 +318,17 @@ export class HomeComponent implements OnInit, OnDestroy {
       if (filenames === undefined || filenames.length !== 1) {
         return;
       }
-      
-      this.images = {};
-      this.objects = {};
+
+      this.canvasObjectService.reset();
       this.sandbox.objects = [];
-      
+
       let filename = filenames[0];
       this.currentFile = filename;
-      
+
       const name = path.basename(this.currentFile);
-      
+
       ipcRenderer.send('lps:start', filename);
-      
+
       this.consoleLog('Starting ' + name);
       this.currentTime = 'Loading ' + name;
       this.isRunning = true;
@@ -461,22 +336,22 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.isDone = false;
     });
   }
-  
+
   ngOnDestroy() {
     ipcRenderer.send('lps:terminate');
   }
-  
+
   @HostListener('window:unload', [ '$event' ])
   unloadHandler(event) {
     ipcRenderer.send('lps:terminate');
   }
-  
+
   @HostListener('window:resize', [ '$event' ])
   resizeHandler(event) {
     this.sandbox.width = window.innerWidth;
     this.sandbox.height = window.innerHeight - timebarHeight;
   }
-  
+
   consoleInputKeypress(event) {
     if (event.keyCode !== 13) {
       return;
