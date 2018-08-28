eaae8c67abc1aeaec44e36fe1906fa56723074a6
Sam Yong <samyonggit@gmail.com>
2018-08-28 22:36:41 +0100

    add buildAboutMenu

---
+++ main/buildAboutMenu.ts
@@ -0,0 +1,128 @@
+import { app, Menu, MenuItemConstructorOptions, shell, BrowserWindow } from 'electron';
+import createMainWindow from './createMainWindow';
+import createAboutWindow from './createAboutWindow';
+
+const REPORT_ISSUE_URL = 'https://github.com/mauris/lps-studio/issues';
+
+export default function buildMainMenu() {
+  let menuTemplate: MenuItemConstructorOptions[] = [];
+
+  if (process.platform === 'darwin') {
+    // for macOS
+    menuTemplate.push({
+      label: 'Menu',
+      submenu: [
+        {
+          label: 'About LPS Studio',
+          click() {
+            createAboutWindow();
+          }
+        },
+        {
+          label: 'View License'
+        },
+        { type: 'separator' },
+        {
+          label: 'Quit',
+          accelerator: 'Cmd+Q',
+          click() {
+            app.quit()
+          }
+        }
+      ]
+    });
+  }
+
+  let fileMenu: MenuItemConstructorOptions[] = [
+    {
+      label: 'New Window',
+      accelerator: 'CmdOrCtrl+N',
+      click() {
+        createMainWindow();
+      }
+    },
+    { type: 'separator' },
+    {
+      label: 'Close Window',
+      accelerator: 'CmdOrCtrl+W',
+      click() {
+        let focusedWindow = BrowserWindow.getFocusedWindow();
+        if (focusedWindow === null) {
+          return;
+        }
+        focusedWindow.close();
+      }
+    },
+    {
+      label: 'Close All Windows',
+      click() {
+        let windows = BrowserWindow.getAllWindows();
+        windows.forEach((window) => {
+          window.close();
+        });
+      }
+    }
+  ];
+
+
+  if (process.platform !== 'darwin') {
+    fileMenu.push({ type: 'separator' });
+    fileMenu.push({
+      label: 'Quit',
+      accelerator: 'Alt+F4',
+      click() {
+        app.quit()
+      }
+    });
+  }
+
+  menuTemplate.push({
+    label: 'File',
+    submenu: fileMenu
+  });
+
+  // help menu
+  if (process.platform === 'darwin') {
+    // for macOS
+    menuTemplate.push({
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Welcome Guide'
+        },
+        {
+          label: 'Report Issue',
+          click() {
+            shell.openExternal(REPORT_ISSUE_URL)
+          }
+        }
+      ]
+    });
+  } else {
+    menuTemplate.push({
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Welcome Guide'
+        },
+        {
+          label: 'Report Issue',
+          click() {
+            shell.openExternal(REPORT_ISSUE_URL)
+          }
+        },
+        { type: 'separator' },
+        {
+          label: 'View License'
+        },
+        {
+          label: 'About LPS Studio'
+        }
+      ]
+    });
+  }
+  // help menu
+
+  return Menu.buildFromTemplate(menuTemplate)
+}
