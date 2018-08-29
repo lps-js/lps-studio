79830bc54dff4d7ca1fc705ce6395cd853465542
Sam Yong <samyonggit@gmail.com>
2018-08-29 02:44:19 +0100

    fix license overrides

--- scripts/license-overrides.js
+++ scripts/license-overrides.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const metadata = require('../package.json');
 
-module.exports = {
+let overrides = {
   'font-awesome@4.7.0': {
     license: 'MIT, SIL OFL 1.1',
     source: 'https://fontawesome.com/v4.7.0/license/',
     sourceText: fs.readFileSync(__dirname + '/licenses/font-awesome.txt', 'utf8')
-  },
-
-  `${metadata.name}@${metadata.version}`: {
-    license: 'BSD-3-Clause, MIT',
-    source: 'https://github.com/mauris/lps-studio/',
-    sourceText: fs.readFileSync(__dirname + '/../LICENSE', 'utf8')
   }
 };
+
+overrides[`${metadata.name}@${metadata.version}`] = {
+  license: 'BSD-3-Clause, MIT',
+  source: 'https://github.com/mauris/lps-studio/',
+  sourceText: fs.readFileSync(__dirname + '/../LICENSE', 'utf8')
+};
+
+module.exports = overrides;
