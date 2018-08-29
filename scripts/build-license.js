70b45a8abcc8826da201a2e47056d7042ba922b7
Sam Yong <samyonggit@gmail.com>
2018-08-29 02:54:33 +0100

    fix build license script

--- scripts/build-license.js
+++ scripts/build-license.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const legalEagle = require('legal-eagle');
 const licenseOverrides = require('./license-overrides.js');
+const metadata = require('../package.json');
 const appRootPath = path.resolve(__dirname, '../');
 const legalEagleOptions = {
   path: appRootPath,
@@ -9,10 +10,16 @@ const legalEagleOptions = {
 };
 const targetOutputFile = '_licenses.json';
 
-legalEagle(legalEagleOptions, (err, summary) => {
+legalEagle(legalEagleOptions, (err, summaryArg) => {
   if (err) {
     console.error(err);
     return;
   }
+  let summary = summaryArg;
+
+  let packageKey = `${metadata.name}@${metadata.version}`;
+  summary[packageKey].license = 'BSD-3-Clause, MIT';
+  summary[packageKey].sourceText = fs.readFileSync(__dirname + '/../LICENSE', 'utf8');
+
   fs.writeFileSync(path.join(appRootPath, targetOutputFile), JSON.stringify(summary));
 });
--- scripts/license-overrides.js
+++ scripts/license-overrides.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const metadata = require('../package.json');
 
 let overrides = {
   'font-awesome@4.7.0': {
@@ -7,12 +6,6 @@ let overrides = {
     source: 'https://fontawesome.com/v4.7.0/license/',
     sourceText: fs.readFileSync(__dirname + '/licenses/font-awesome.txt', 'utf8')
   }
-};
-
-overrides[`${metadata.name}@${metadata.version}`] = {
-  license: 'BSD-3-Clause, MIT',
-  source: 'https://github.com/mauris/lps-studio/',
-  sourceText: fs.readFileSync(__dirname + '/../LICENSE', 'utf8')
 };
 
 module.exports = overrides;
